9e85b58e778db93a8d83009bc4f20d841866846c
sudarshan161219 <godhustler90956@gmail.com>
2024-10-03 19:06:18 +0530

    code refactor

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ import recipeData from "./Instructions.json"  with { type: "json" };
 const InstructionsContainer = document.querySelector(".instrc-container");
 
 recipeData.steps.map((e) => {
-InstructionsContainer.innerHTML += `<div class="instrc-info">
-<span class="instrc-info-span">${e.step}.</span>
-<div class="instrc-span-para"> 
-<p class="instrc-para"><span class="span-bold-text2">${e.title}: </span>  ${e.description }</p>
-</div>
- </div>`;
-});
+   InstructionsContainer.innerHTML += `
+     <li class="instrc-info">
+       <span class="instrc-info-span">${e.step}.</span>
+       <p class="instrc-para">
+         <span class="span-bold-text2">${e.title}: </span> ${e.description}
+       </p>
+     </li>`;
+ });
+ 
 
 
-console.log(recipeData.steps);
